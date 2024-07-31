feed9c76e990c816f8fb1fcd6c3ce0c4e211a99e
RishabhS05 <rishabhshri2795@gmail.com>
2024-07-31 14:49:53 +0530

    github project nitial project with Search screen and favorite screen  with programatic  ui.

---
+++ GithubProjectTutorial/widgets/GptButton.swift
@@ -0,0 +1,20 @@
+//
+//  GptButton.swift
+//  GithubProjectTutorial
+//
+//  Created by Rishabh Shrivastava on 31/07/24.
+//
+
+import UIKit
+
+class GptButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GithubProjectTutorial/widgets/GptTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GptTextField.swift
+//  GithubProjectTutorial
+//
+//  Created by Rishabh Shrivastava on 31/07/24.
+//
+
+import UIKit
+
+class GptTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
